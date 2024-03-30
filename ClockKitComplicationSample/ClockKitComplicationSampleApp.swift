ce0a8647d1e8df930745825114defc0cc21ebb2c
Jaeho Yoo <y73447jh@gmail.com>
2024-03-30 16:27:04 +0900

    Initial Commit

---
+++ ClockKitComplicationSample/ClockKitComplicationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockKitComplicationSampleApp.swift
+//  ClockKitComplicationSample
+//
+//  Created by 유재호 on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ClockKitComplicationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
